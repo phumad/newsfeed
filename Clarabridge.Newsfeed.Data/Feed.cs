0b7c3dc230028530323b9bb0ad6de4e3e05d21c7
pankaj <pankajhumad@hotmail.com>
2016-10-13 06:49:40 -0400

    news field size to max

--- Clarabridge.Newsfeed.Data/Feed.cs
+++ Clarabridge.Newsfeed.Data/Feed.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,10 @@ namespace Clarabridge.Newsfeed.Data
     {
         [Key]
         public int Id { get; set; }
+
         public DateTime DateCreated { get; set; }
+
+        [Column(TypeName = "nvarchar(max)")]
         public string Body { get; set; }
     }
 }
